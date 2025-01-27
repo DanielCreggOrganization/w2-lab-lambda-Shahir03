160bb636737b250b4c23e1a3277f54244fa82e87
Shahir03 <148056693+Shahir03@users.noreply.github.com>
2025-01-27 13:39:36 +0000

    runnable added to main

--- src/ie/atu/lambda/Main.java
+++ src/ie/atu/lambda/Main.java
@@ -5,6 +5,11 @@ public class Main {
         StringOperation toUpperCase = (String input) -> input.toUpperCase(); // Create instance of ToUpperCase
         String result = toUpperCase.apply("hello world!"); // Test with a string
         System.out.println("Result: " + result); // Output: HELLO WORLD!
+        
+        //part 3
+        Runnable helloRunnable = () -> System.out.println("Hello from the thread");
+        Thread thread = new Thread(helloRunnable);
+        thread.start(); 
     }
     
 }
